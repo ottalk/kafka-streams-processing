5f08c3d5aa183981569d81df8dc5eaf9a73ec851
Kuldip Ottal <kuldip.ottal@gmail.com>
2023-04-23 13:28:44 +0100

    Latest updates

--- five-min-window/src/main/java/com/harvicom/kafkastreams/fiveminwindow/processor/FiveMinWindowEventStreamProcessor.java
+++ five-min-window/src/main/java/com/harvicom/kafkastreams/fiveminwindow/processor/FiveMinWindowEventStreamProcessor.java
@@ -7,6 +7,8 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.Windowed;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,10 +22,16 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.connect.json.JsonDeserializer;
 import org.apache.kafka.connect.json.JsonSerializer;
+import org.apache.kafka.streams.kstream.TimeWindowedSerializer;
+import org.apache.kafka.streams.kstream.TimeWindowedDeserializer;
+
 import org.apache.kafka.streams.kstream.KeyValueMapper;
 import org.apache.kafka.streams.KeyValue;
+import java.time.Duration;
 
 @Component
 public class FiveMinWindowEventStreamProcessor {
@@ -37,6 +45,15 @@ public class FiveMinWindowEventStreamProcessor {
         Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
         Serializer<JsonNode> jsonSerializer = new JsonSerializer();
         Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
+
+        StringSerializer stringSerializer = new StringSerializer();
+        StringDeserializer stringDeserializer = new StringDeserializer();
+        Serde<String> stringSerde = Serdes.serdeFrom(stringSerializer,stringDeserializer);
+
+        TimeWindowedSerializer<String> windowedSerializer = new TimeWindowedSerializer<>(stringSerializer);
+        TimeWindowedDeserializer<String> windowedDeserializer = new TimeWindowedDeserializer<>();
+        Serde<Windowed<String>> windowedSerde = Serdes.serdeFrom(windowedSerializer,windowedDeserializer);
+
 
         //Consumed<String, JsonNode> consumerOptions = Consumed.with(Serdes.String(), jsonSerde).withTimestampExtractor(new StringTimestampExtractor());
         //KStream<String, JsonNode> kStream = streamsBuilder.stream("streams-test-1", consumerOptions);
@@ -55,8 +72,11 @@ public class FiveMinWindowEventStreamProcessor {
         }).to("streams-test-2", Produced.with(Serdes.String(),jsonSerde));
 
         //KGroupedStream<String, JsonNode> group = kStream.groupByKey(Grouped.with(Serdes.String(), jsonSerde));
+/*
+        TimeWindows tumblingWindow = TimeWindows.ofSizeAndGrace(Duration.ofMinutes(1), Duration.ofSeconds(10));
+     
 
-        KTable<String, JsonNode> countAndSum = kStream.groupByKey(Grouped.with(Serdes.String(), jsonSerde)).aggregate(
+        KTable<Windowed<String>, JsonNode> countAndSum = kStream.groupByKey(Grouped.with(Serdes.String(), jsonSerde)).windowedBy(tumblingWindow).aggregate(
                 () -> {
                     ObjectNode node = JsonNodeFactory.instance.objectNode();
                     node.put("count", 0);
@@ -67,9 +87,7 @@ public class FiveMinWindowEventStreamProcessor {
                 (key, value, aggregate) -> {
                     // if we want to clear the state when we get the "last" event of this sort
                     // we can "just" return null here
-                    /*
-                    if (eventLastOfItskind) return null;
-                    */
+
                     //((ObjectNode)aggregate).put("WINDOW_START", key.getBytes().toString());
                     ((ObjectNode)aggregate).put("MERCHANT_NO", value.get("MERCHANT_NO").asText());
                     ((ObjectNode)aggregate).put("DIV", value.get("DIV").asText());
@@ -79,10 +97,12 @@ public class FiveMinWindowEventStreamProcessor {
                     ((ObjectNode)aggregate).put("max", Math.max(aggregate.get("max").asDouble(),value.get("RESPONSE_TIME").asDouble()));
                     return aggregate;
                 },
-                Materialized.with(Serdes.String(),jsonSerde));
+                Materialized.with(windowedSerde,jsonSerde)));
+
+                //Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("aggregated-stream-store") .withValueSerde(Serdes.Long())
 
         
-        KTable<String, JsonNode> average = countAndSum.mapValues((value -> {ObjectNode node = JsonNodeFactory.instance.objectNode();
+        KTable<Windowed<String>, JsonNode> average = countAndSum.mapValues((value -> {ObjectNode node = JsonNodeFactory.instance.objectNode();
             node.put("MERCHANT_NO", value.get("MERCHANT_NO").asText());
             node.put("DIV", value.get("DIV").asText());
             node.put("COUNTRY", value.get("COUNTRY").asText());
@@ -93,6 +113,7 @@ public class FiveMinWindowEventStreamProcessor {
         }));
                 
         //average.to(Serdes.String(), jsonSerde, "streams-test-2");
-        average.toStream().to("streams-test-3", Produced.with(Serdes.String(), jsonSerde));
+        average.toStream().to("streams-test-3", Produced.with(windowedSerde, jsonSerde));
+*/
     }
 }
